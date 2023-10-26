3e398ae93eee27664bac88fb1681bf51c97ebcd0
Vladimir Varankin <vladimir@varank.in>
2023-10-26 13:56:15 +0200

    parse lists

--- cmd/mybittorrent/main.go
+++ cmd/mybittorrent/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"os"
 	"strconv"
@@ -13,29 +14,49 @@ import (
 // Example:
 // - 5:hello -> hello
 // - 10:hello12345 -> hello12345
-func decodeBencode(s string) (interface{}, error) {
+func decodeBencode(s string) (interface{}, string, error) {
 	tag := s[0]
 	switch {
 	case unicode.IsDigit(rune(tag)):
 		head, tail, ok := strings.Cut(s, ":")
 		if !ok {
-			return nil, fmt.Errorf("can't find colon in %q", s)
+			return nil, "", fmt.Errorf("can't find colon in %q", s)
 		}
 
 		slen, err := strconv.Atoi(head)
 		if err != nil {
-			return "", err
+			return "", "", err
 		}
 
-		return tail[:slen], nil
+		return tail[:slen], tail[slen:], nil
 	case tag == 'i':
-		head, _, ok := strings.Cut(s[1:], "e")
+		head, tail, ok := strings.Cut(s[1:], "e")
 		if !ok {
-			return nil, fmt.Errorf("can't find end of %q", s)
+			return nil, "", fmt.Errorf("can't find end of %q", s)
 		}
-		return strconv.Atoi(head)
+		n, err := strconv.Atoi(head)
+		return n, tail, err
+	case tag == 'l':
+		var list []interface{}
+		s = s[1:]
+		for s != "" {
+			var (
+				v   interface{}
+				err error
+			)
+			v, s, err = decodeBencode(s)
+			if err != nil {
+				return nil, s, err
+			}
+			list = append(list, v)
+			// consume the end of the list
+			if s[0] == 'e' {
+				s = s[1:]
+			}
+		}
+		return list, s, nil
 	default:
-		return "", fmt.Errorf("only strings are supported at the moment")
+		return "", s, errors.ErrUnsupported
 	}
 }
 
@@ -43,10 +64,14 @@ func main() {
 	command := os.Args[1]
 
 	if command == "decode" {
-		s := os.Args[2]
-		val, err := decodeBencode(s)
+		input := os.Args[2]
+		val, tail, err := decodeBencode(input)
 		if err != nil {
-			fmt.Println(err)
+			fmt.Printf("%s: %s", tail, err)
+			return
+		}
+		if tail != "" {
+			fmt.Printf("didn't consume the whole input: tail %q", tail)
 			return
 		}
 
